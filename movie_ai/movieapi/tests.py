eebe600eae74b583f373f8af27703865a6a387f3
captainnemo9292 <captainnemo9292@gmail.com>
2024-05-03 12:26:15 +0000

    fix test

--- movie_ai/movieapi/tests.py
+++ movie_ai/movieapi/tests.py
@@ -1,34 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
 
-class MovieAPITestCase(TestCase):
-    def setUp(self):
-        self.client = APIClient()
+class MovieAPITestCase(APITestCase):
 
     def test_short_form_1(self):
-        response = self.client.post('/short_form/', {"query": "Scenes of the Wizard."}, format='json')
+        response = self.client.post(reverse('short_form'), {"query": "Scenes of the Wizard."}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_short_form_2(self):
-        response = self.client.post('/short_form/', {"query": "Scene where the Witch melts."}, format='json')
+        response = self.client.post(reverse('short_form'), {"query": "Scene where the Witch melts."}, format='json')
         resp_timestamp = response.json()["timestamp"]
         oracle_timestampe = {"start":"02:39","end":"03:12"}
         self.assertEqual(resp_timestamp, oracle_timestampe)
 
     def test_video_qa_1(self):
-        response = self.client.post('/video_qa/', {"query":"How did the witch die?"}), format='json'
+        response = self.client.post(reverse('video_qa'), {"query":"How did the witch die?"}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_video_qa_2(self):
-        response = self.client.post('/video_qa/', {"query":"What did the Wizard tell Dorothy to do?"}, format='json')
+        response = self.client.post(reverse('video_qa'), {"query":"What did the Wizard tell Dorothy to do?"}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_avatar_chat_1(self):
-        response = self.client.post('/avatar_chat/', {"query": "What did it feel like to encounter the Wizard?", "Character": "Dorothy"}, format='json')
+        response = self.client.post(reverse('avatar_chat'), {"query": "What did it feel like to encounter the Wizard?", "character": "Dorothy"}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_avatar_chat_2(self):
-        response = self.client.post('/avatar_chat/', {"query": "How did melting feel like?", "Character": "Witch"}, format='json')
+        response = self.client.post(reverse('avatar_chat'), {"query": "How did melting feel like?", "character": "Witch"}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-

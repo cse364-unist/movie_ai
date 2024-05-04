b271591505c0ef5bac7b25d494c13109627592a9
류병우 <captainnemo9292@gmail.com>
2024-05-04 13:40:56 +0900

    Update tests.py

--- movie_ai/movieapi/tests.py
+++ movie_ai/movieapi/tests.py
@@ -15,6 +15,10 @@ class MovieAPITestCase(APITestCase):
         oracle_timestampe = {"start":"02:39","end":"03:12"}
         self.assertEqual(resp_timestamp, oracle_timestampe)
 
+    def test_short_form_3(self):
+        response = self.client.post(reverse('short_form'), {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_video_qa_1(self):
         response = self.client.post(reverse('video_qa'), {"query":"How did the witch die?"}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -23,6 +27,10 @@ class MovieAPITestCase(APITestCase):
         response = self.client.post(reverse('video_qa'), {"query":"What did the Wizard tell Dorothy to do?"}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_video_qa_3(self):
+        response = self.client.post(reverse('video_qa'), {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_avatar_chat_1(self):
         response = self.client.post(reverse('avatar_chat'), {"query": "What did it feel like to encounter the Wizard?", "character": "Dorothy"}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -30,3 +38,7 @@ class MovieAPITestCase(APITestCase):
     def test_avatar_chat_2(self):
         response = self.client.post(reverse('avatar_chat'), {"query": "How did melting feel like?", "character": "Witch"}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_avatar_chat_3(self):
+        response = self.client.post(reverse('avatar_chat'), {"character": "Witch"}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
